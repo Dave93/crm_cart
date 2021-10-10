c1db2e2d07f0b2fa297066fd5cc8c6f090ed6595
Davron <yul.davron.93@gmail.com>
2021-10-10 18:23:34 +0500

    set pm2 process name

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 module.exports = {
   apps: [
     {
-      name: 'crm_cart',
+      name: process.env.PM2_APP_NAME,
       script: './server.js',
       watch: true,
       env: {
