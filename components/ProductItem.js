80c8a2c348eda945bc00d703d673ce8409e4e0f2
Davron Yuldashev <davr@Davrons-Mac-Pro.local>
2025-03-11 14:18:53 +0500

    added additional button to product list

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -37,7 +37,7 @@ const ProductItem = ({ style, product, loadCart, loadRelatedItems }) => {
     setChosenModifiers(mods);
   };
 
-  const addToCart = async () => {
+  const addToCart = async (additional = false) => {
     setIsAddingCart(true);
     const urlParams = new URLSearchParams(window.location.search);
     let project = urlParams.get("project");
@@ -58,6 +58,9 @@ const ProductItem = ({ style, product, loadCart, loadRelatedItems }) => {
     }
     if (fuser) {
       rowData.fuser = fuser;
+    }
+    if (additional) {
+      rowData.additional = true;
     }
     const { data } = await axios.post(
       `https://${publicRuntimeConfig.crmUrl}/rest/1/63dif6icpi61ci3f/add.deal.basket.item`,
@@ -109,6 +112,38 @@ const ProductItem = ({ style, product, loadCart, loadRelatedItems }) => {
                         precision: 0,
                       }).format()}
                     </div>
+                <button
+                  className="bg-blue-400 px-4 py-2 mr-2 text-white uppercase rounded-md w-48 text-center"
+                  onClick={() => addToCart(true)}
+                  disabled={isAddingCart}
+                >
+                  {isAddingCart ? (
+                    <span>
+                      <svg
+                        className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <circle
+                          className="opacity-25"
+                          cx="12"
+                          cy="12"
+                          r="10"
+                          stroke="currentColor"
+                          stroke-width="4"
+                        ></circle>
+                        <path
+                          className="opacity-75"
+                          fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                      </svg>
+                    </span>
+                  ) : (
+                    <span>Доп. продажа</span>
+                  )}
+                </button>
                     <div className="w-60 text-right">
                       <Disclosure.Button className="bg-blue-400 px-3 py-2 flex rounded-md items-center uppercase text-white">
                         <span>Выбрать модификаторы</span>
@@ -246,8 +281,40 @@ const ProductItem = ({ style, product, loadCart, loadRelatedItems }) => {
                   precision: 0,
                 }).format()}
               </div>
-
+              <button
+                  className="bg-blue-400 px-4 py-2 mr-2 text-white uppercase rounded-md w-48 text-center"
+                  onClick={() => addToCart(true)}
+                  disabled={isAddingCart}
+                >
+                  {isAddingCart ? (
+                    <span>
+                      <svg
+                        className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <circle
+                          className="opacity-25"
+                          cx="12"
+                          cy="12"
+                          r="10"
+                          stroke="currentColor"
+                          stroke-width="4"
+                        ></circle>
+                        <path
+                          className="opacity-75"
+                          fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                      </svg>
+                    </span>
+                  ) : (
+                    <span>Доп. продажа</span>
+                  )}
+                </button>
               <div className="w-60 text-right">
+                
                 <button
                   className="bg-blue-400 px-3 py-2 rounded-md uppercase text-white w-48 text-center"
                   onClick={addToCart}
